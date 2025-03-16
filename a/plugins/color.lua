f20d665f6ec8e7b53361e91aac9649a96c0fa869
ankutmukhia <ankitmukhia157@gmail.com>
2025-03-16 16:41:27 +0530

    config change

---
+++ a/plugins/color.lua
@@ -0,0 +1,45 @@
+return {
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require('kanagawa').setup({
+			compile = false,
+			undercurl = false,
+			commentStyle = { italic = true },
+			functionStyle = {},
+			keywordStyle = { italic = false},
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false,
+			dimInactive = false,
+			terminalColors = false,
+			colors = {
+				palette = {},
+				theme = {
+					wave = {},
+					lotus = {},
+					dragon = {},
+					all = {
+						ui = {
+							bg_gutter = "none"
+						},
+						diag = {
+							-- error = "#FF8080"
+							error = "#c4746e"
+						}
+					},
+				},
+			},
+			overrides = function(colors) -- add/modify highlights
+				return {}
+			end,
+			theme = "wave",              -- Load "wave" theme when 'background' option is not set
+			background = {                 -- map the value of 'background' option to a theme
+				dark = "dragon",       -- try "dragon" !
+				light = "lotus"
+			},
+		})
+
+		-- setup must be called before loading
+		vim.cmd("colorscheme kanagawa")
+	end
+}
---
+++ a/plugins/telescope.lua
@@ -0,0 +1,5 @@
+return {
+	'nvim-telescope/telescope.nvim',
+	config = function ()
+	end
+}
